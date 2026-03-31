520111cd8d57e2ea83222fcb39a13749efebe1e4
Benitezcesaret32 <cesar.benitezet32@gmail.com>
2026-03-31 12:43:05 -0300

    Add files via upload

---
+++ tp4.cs
@@ -0,0 +1,30 @@
+﻿//ejercicio 1
+for (int a = 0; a <= 10; a++)
+{
+    Console.WriteLine(a);
+}
+//ejercicio 2
+for (int a = 0; a <= 5; a++)
+{
+    Console.WriteLine("hola mundo");
+}
+//ejercicio 3
+for (int a = 2; a <= 20; a++)
+{
+   if (a % 2 == 0)
+    {
+        Console.WriteLine(a);
+    }
+}
+//ejercicio 4
+for (int a = 1; a <= 12; a++)
+{
+    Console.WriteLine(a * 7);
+}
+//ejercicio 5
+int sumar = 0;
+for (int a = 1; a <= 5; a++)
+{
+    sumar += a;
+    Console.WriteLine(sumar);
+}
