9bf511fe110e4dcaf20bcc07958804edcf009690
Benitezcesaret32 <cesar.benitezet32@gmail.com>
2026-04-23 11:13:45 -0300

    Update TP cifrado César.cs

--- TP cifrado César.cs
+++ TP cifrado César.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("ingrese una frase para cifrarla o descifrarla sin caracteres especiales");
+﻿﻿Console.WriteLine("ingrese una frase para cifrarla o descifrarla sin caracteres especiales");
 string frase;
 while (true)
 {
@@ -25,18 +25,8 @@ while (true)
 }
 
 Console.WriteLine("ingrese el numero de desplazamiento: ");
-string entradaDesplazamiento = Console.ReadLine();
-int desplazamiento;
-bool esNumeroValido = int.TryParse(entradaDesplazamiento, out desplazamiento);
-if (esNumeroValido)
-{
-    Console.WriteLine("Número de desplazamiento válido: " + desplazamiento);
-}
-else
-{
-    Console.WriteLine("Número de desplazamiento no válido. Por favor, ingrese un número entero.");
-    return;
-}
+int desplazamiento = Convert.ToInt32(Console.ReadLine());
+
 static string Cifrar(string texto, int desplazamiento)
 {
     desplazamiento = desplazamiento % 26;
@@ -44,21 +34,11 @@ static string Cifrar(string texto, int desplazamiento)
     for (int i = 0; i < texto.Length; i++)
     {
         char letra = texto[i];
-        if (letra >= 'A' && letra <= 'Z')
-        {
-            char baseLetra = 'A';
-            char nueva = (char)(((letra - baseLetra + desplazamiento + 26) % 26) + baseLetra);
-            resultado += nueva;
-        }
-        else if (letra >= 'a' && letra <= 'z')
+        if (letra >= 'a' && letra <= 'z')
         {
             char baseLetra = 'a';
             char nueva = (char)(((letra - baseLetra + desplazamiento + 26) % 26) + baseLetra);
             resultado += nueva;
-        }
-        else
-        {
-            resultado += letra;
         }
     }
     return resultado;
@@ -72,21 +52,11 @@ static string Descifrar(string texto, int desplazamiento)
     for (int i = 0; i < texto.Length; i++)
     {
         char letra = texto[i];
-        if (letra >= 'A' && letra <= 'Z')
-        {
-            char baseLetra = 'A';
-            char nueva = (char)(((letra - baseLetra - desplazamiento + 26) % 26) + baseLetra);
-            resultado += nueva;
-        }
-        else if (letra >= 'a' && letra <= 'z')
+        if (letra >= 'a' && letra <= 'z')
         {
             char baseLetra = 'a';
             char nueva = (char)(((letra - baseLetra - desplazamiento + 26) % 26) + baseLetra);
             resultado += nueva;
-        }
-        else
-        {
-            resultado += letra;
         }
     }
     return resultado;
@@ -101,13 +71,13 @@ for (int i = 0; i < frase.Length; i++)
     }
 }
 Console.WriteLine("ingrese una frase para cifrarla o descifrarla: (c/d)");
-string opcion = Console.ReadLine();
-if (opcion.ToLower() == "c")
+string opcion = Console.ReadLine().ToLower();
+if (opcion == "c")
 {
     string cifrado = Cifrar(frase, desplazamiento);
     Console.WriteLine("Frase cifrada: " + cifrado);
 }
-else if (opcion.ToLower() == "d")
+else if (opcion == "d")
 {
     string descifrado = Descifrar(frase, desplazamiento);
     Console.WriteLine("Frase descifrada: " + descifrado);
