038f979ae65cb327efe74470d0f62b329d6a22d2
Benitezcesaret32 <cesar.benitezet32@gmail.com>
2026-04-30 09:05:29 -0300

    Add files via upload

---
+++ TP While.cs
@@ -0,0 +1,17 @@
+﻿//ejercicio 1
+Console.WriteLine ("ingrese 0 o 1");
+int numero = Convert.ToInt32(Console.ReadLine());
+while (numero != 0 && numero != 1)
+{
+    Console.WriteLine("ingrese 0 o 1");
+    numero = Convert.ToInt32(Console.ReadLine());
+}
+
+//ejercicio 2
+Console.WriteLine("ingrese un numero de dos cifras");
+int numero2 = Convert.ToInt32(Console.ReadLine());
+while (numero2 < 10 || numero2 > 99)
+{
+    Console.WriteLine("ingrese un numero de dos cifras");
+    numero2 = Convert.ToInt32(Console.ReadLine());
+}
